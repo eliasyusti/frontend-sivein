ce264da2712c31e4db07c8ce79818f8ab40e188d
eliasyusti <eliasyusti@gmail.com>
2023-11-10 03:47:34 -0300

    edit complete, there that to prove

--- src/pages/products/getProducts/getProducts.js
+++ src/pages/products/getProducts/getProducts.js
@@ -9,6 +9,7 @@ import CreateProducts from "../createProducts/createProducts";
 
 export default function DenseTable() {
   const [open, setOpen] = React.useState(false);
+  const [valuesForEdit, setValuesForEdit] = React.useState({});
   const dispatch = useDispatch();
   const productsData = useCallback(async () => {
     await dispatch(getListProducts());
@@ -19,6 +20,7 @@ export default function DenseTable() {
   }, [productsData]);
 
   const handleClickOpen = () => {
+    setValuesForEdit({});
     setOpen(true);
   };
 
@@ -29,13 +31,32 @@ export default function DenseTable() {
   const Products = useSelector((state) => state.Products);
 
   const data = Products.map((product) => ({
+    id: product.id,
     name: product.name,
     description: product.description,
     price: product.price,
     category: product.category,
   }));
 
+  const handleEditProducts = (body) => {
+    const [productEdit] = Products.filter((item) => item.id === body[0]);
+    // console.log(body, "body");
+    // console.log(productEdit, "productEdit");
+    setValuesForEdit({
+      id: productEdit.id,
+      name: body[1],
+      description: body[2],
+      price: body[3],
+      category: body[4],
+    });
+    setOpen(true);
+  };
+
   const columns = [
+    {
+      name: "id",
+      options: { print: false, display: false },
+    },
     {
       name: "name",
       label: "NOMBRE",
@@ -55,10 +76,17 @@ export default function DenseTable() {
     {
       name: "ACCIONES",
       options: {
-        filter: true,
+        filter: false,
         customBodyRender: (value, tableMeta, updateValue) => {
+          //console.log(tableMeta);
           return (
-            <Button variant="contained" color="secondary">
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={() => {
+                handleEditProducts(tableMeta.rowData);
+              }}
+            >
               <EditIcon />
             </Button>
           );
@@ -88,7 +116,11 @@ export default function DenseTable() {
         <Grid item xs={12}>
           <MUIDataTable columns={columns} data={data} options={options} />
         </Grid>
-        <CreateProducts open={open} handleClose={handleClose} />
+        <CreateProducts
+          valuesForEdit={valuesForEdit}
+          open={open}
+          handleClose={handleClose}
+        />
       </Grid>
     </>
   );
